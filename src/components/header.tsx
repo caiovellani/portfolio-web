bb37ee21728ae00e61573c08bf45d9db82267307
Caio Vellani <caiovellani@outlook.com>
2025-04-11 19:13:35 -0300

    refactor(header): Refactor the header component
    - Moving the navigation menu logic to a dedicated `Navbar` component
    - Updating the header layout to be more responsive
    - Implementing active state styling for navigation items
    - Replacing the old logo with the new one
    - Simplifying the overall structure of the header
    - Using clsx for conditional classNames in the navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,53 +1,26 @@
-'use client'
-
-import { useState } from 'react'
-import { Menu, X } from 'lucide-react'
-import logoImg from '@/../public/logo2.png'
 import Image from 'next/image'
+import logo from '@/../public/logo.png'
+import { Navbar } from '@/components/navbar'
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false)
-
   return (
-    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-      <div className="max-w-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
-        <Image src={logoImg} alt="Logo" height={50} width={50} />
-
-        <div className="relative md:justify-self-center">
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="md:hidden w-10 h-10 grid place-items-center bg-zinc-50/10 rounded-xl ring-inset ring-1 ring-zinc-50/[0.02] backdrop-blur-2xl hover:bg-zinc-50/15 transition-[transform,background-color] active:scale-95"
-          >
-            {isOpen ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
-          </button>
-
-          <nav
-            className={`
-              md:static md:flex md:items-center md:mt-0 md:opacity-100 md:scale-100 md:visible md:blur-0
-              absolute top-full mt-2 right-0 min-w-40 p-2 bg-zinc-50/10 rounded-2xl ring-inset ring-1 ring-zinc-50/5 backdrop-blur-2xl isolate transition-all duration-300
-              ${
-                isOpen
-                  ? 'opacity-100 scale-100 blur-0 visible'
-                  : 'opacity-0 scale-90 blur-sm invisible'
-              }
-              md:transition-none
-            `}
-          >
-            {['Home', 'About', 'Work', 'Reviews', 'Contact'].map((item) => (
-              <a
-                key={item}
-                className="grid items-center h-9 px-4 text-sm font-medium tracking-wide text-zinc-50/50 hover:text-zinc-50 transition-colors active:text-zinc-900  active:absolute active:top-2 active:left-2 active:right-2 active:h-9 active:bg-zinc-50 active:rounded-lg active:-z-10 active:transition-[top,left] active:duration-500"
-                href=""
-              >
-                {item}
-              </a>
-            ))}
-          </nav>
+    <header className="fixed p-4 top-0 left-0 w-full h-20 z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+      <div className="max-w-6xl mx-auto px-4 h-full flex items-center justify-between">
+        {/* Logo */}
+        <div className="flex items-center gap-2">
+          <Image src={logo} alt="Logo" width={55} height={55} />
         </div>
-
-        <a href="#contact" className="">
-          Contact Me
-        </a>
+        <div>
+          <Navbar />
+        </div>
+        <div className="hidden md:flex">
+          <button
+            // onClick={handleSendEmail}
+            className="rounded-xl bg-white text-black p-2 text-sm font-medium h-9 items-center ring-1 ring-zinc-50/5 ring-inset transition-[background-color]"
+          >
+            Contact Me
+          </button>
+        </div>
       </div>
     </header>
   )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,58 @@
+'use client'
+
+import clsx from 'clsx'
+import { Menu, X } from 'lucide-react'
+import { useState } from 'react'
+
+const navItems = [
+  { name: 'Home' },
+  { name: 'About' },
+  { name: 'Work' },
+  { name: 'Reviews' },
+]
+
 export function Navbar() {
-  return
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className="relative md:justify-self-center">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-10 md:hidden h-10 grid place-items-center bg-zinc-50/10 rounded-xl ring-inset ring-zinc-50/[0.02] backdrop-blur-2xl hover:bg-zinc-50/15 transition-[transform,background-color] active:scale-95"
+      >
+        {isOpen ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
+      </button>
+      <nav
+        className={clsx(
+          `absolute top-full mt-2 right-0 min-w-40 p-2 rounded-2xl ring-inset ring-1 ring-zinc-50/5 bg-zinc-900/80 isolate
+           transition-all duration-300 scale-90 opacity-0 blur-0 invisible
+           md:static md:flex md:items-center md:mt-0 md:opacity-100 md:scale-100 md:visible md:transition-none`,
+          isOpen && 'opacity-100 scale-100 visible'
+        )}
+      >
+        {navItems.map((item) => {
+          const isActive = item.name === 'Home'
+
+          return (
+            <div key={item.name} className="relative">
+              {isActive && (
+                <span className="absolute top-0 left-0 right-0 h-9 bg-white rounded-lg -z-10 transition-all duration-500" />
+              )}
+              <a
+                href="#"
+                className={clsx(
+                  'grid items-center h-9 px-4 text-sm font-medium tracking-wide transition-colors',
+                  isActive
+                    ? 'text-zinc-900'
+                    : 'text-zinc-50/50 hover:text-zinc-50'
+                )}
+              >
+                {item.name}
+              </a>
+            </div>
+          )
+        })}
+      </nav>
+    </div>
+  )
 }
