ae11a8be4c27158d90b05599e16867e8e6deafe7
Caio Vellani <caiovellani@outlook.com>
2025-04-16 18:58:57 -0300

    feat(navbar): Add background grid and update navbar styling
    - Adding a background grid element for visual depth
    - Changing the navbar background color from `bg-zinc-950` to `bg-zinc-900` for better contrast

--- src/app/(pages)/components/navbar.tsx
+++ src/app/(pages)/components/navbar.tsx
@@ -19,6 +19,8 @@ export function Navbar() {
 
   return (
     <div className="relative md:justify-self-center">
+      <div className="bg-grid absolute top-0 left-0 right-0 bottom-0 z-[-1]" />
+
       <button
         onClick={() => setIsOpen(!isOpen)}
         className="w-10 md:hidden h-10 grid place-items-center bg-zinc-50/10 rounded-xl ring-inset ring-zinc-50/[0.02] backdrop-blur-2xl hover:bg-zinc-50/15 transition-[transform,background-color] active:scale-95"
@@ -27,7 +29,7 @@ export function Navbar() {
       </button>
       <nav
         className={clsx(
-          `absolute top-full mt-2 right-0 min-w-40 p-2 rounded-2xl ring-inset ring-1 ring-zinc-50/5 bg-zinc-950 isolate
+          `absolute top-full mt-2 right-0 min-w-40 p-2 rounded-2xl ring-inset ring-1 ring-zinc-50/5 bg-zinc-900 isolate
            transition-all duration-300 scale-90 opacity-0 blur-0 invisible
            md:static md:flex md:items-center md:mt-0 md:opacity-100 md:scale-100 md:visible md:transition-none`,
           isOpen && 'opacity-100 scale-100 visible'
