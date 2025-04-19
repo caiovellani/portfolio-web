b85c1079dfc1c6f0c3fd45e13b2d52dbd1c1ab07
Caio Vellani <caiovellani@outlook.com>
2025-04-19 20:02:18 -0300

    feat(projects): Add  project card component to Projects section
    - Importing the ArrowRightIcon from lucide-react for the card's link button
    - Enhancing the Projects section with flex layout and font styling
    - Adding a project card template with:
    1. Placeholder for project image
    2. Technology tag section (with NextJS example)
    3. Project name and description fields
    4. "Code" link with arrow icon
    - Implementing hover effects and proper spacing for the card component

--- src/app/(pages)/components/projects.tsx
+++ src/app/(pages)/components/projects.tsx
@@ -1,12 +1,44 @@
+import { ArrowRightIcon } from 'lucide-react'
+
 export function Projects() {
   return (
-    <section id="projects" className="min-h-screen w-full container mx-auto">
+    <section
+      id="projects"
+      className="min-h-screen justify-center items-center flex flex-col w-full container mx-auto font-poppins"
+    >
       <div className="flex flex-col items-center justify-center p-4">
         <h1 className="text-[48px] font-bold xl:text-[80px]">Projects</h1>
         <p className="text-zinc-50/60 text-center text-base font-medium leading-tight">
           I have worked on a wide range of web application projects. Here are
           some of my projects.
         </p>
+      </div>
+
+      <div className="bg-[#18181B] border mt-20  border-accent rounded-2xl p-4 flex flex-col gap-4 w-full max-w-sm hover:scale-[1.01] transition-transform duration-300">
+        <div className="bg-[#1C1C22] rounded-xl w-full h-48 flex items-center justify-center text-white font-bold text-lg">
+          PROJECT PHOTO
+        </div>
+
+        <div className=" flex flex-wrap gap-2 justify-center">
+          <span className="border border-accent text-zinc-300 px-4 py-1 bg-[#1c1c22] rounded-4xl text-sm">
+            NextJS
+          </span>
+        </div>
+
+        <div>
+          <h1 className="text-zinc-100 text-xl font-bold">Project name</h1>
+          <p className="text-zinc-400 font-medium">Descrição do projeto</p>
+        </div>
+        <div className="flex justify-end mt-auto">
+          <a
+            target="_blank"
+            href="#"
+            className="text-accent font-semibold flex items-center justify-center gap-2"
+          >
+            Code
+            <ArrowRightIcon className="w-5 h-5" />
+          </a>
+        </div>
       </div>
     </section>
   )
