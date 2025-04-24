ecd0aeb513d31611544d534df7f1acb125563164
Caio Vellani <caiovellani@outlook.com>
2025-04-24 17:59:26 -0300

    refactor(projects): Added `p-4` padding to ensure consistent spacing with other sections

--- src/app/(pages)/components/projects.tsx
+++ src/app/(pages)/components/projects.tsx
@@ -4,7 +4,7 @@ export function Projects() {
   return (
     <section
       id="projects"
-      className="min-h-screen justify-center items-center flex flex-col w-full container mx-auto font-poppins"
+      className="min-h-screen justify-center items-center flex flex-col p-4 w-full container mx-auto font-poppins"
     >
       <div className="flex flex-col items-center justify-center p-4">
         <h1 className="text-[48px] font-bold xl:text-[80px]">Projects</h1>
