5375f6bfc6ce712f35691c42c230d9ffbac970b0
Caio Vellani <caiovellani@outlook.com>
2025-04-24 18:24:13 -0300

    feat(experience): Implement Experience section with dynamic card rendering
    - Created a new Experience section with proper layout and styling
    - Added an array of experience data with job details
    - Implemented dynamic rendering of ExperienceCard components based on the experience data
    - Included descriptive section heading and introduction text

--- src/app/(pages)/components/experience.tsx
+++ src/app/(pages)/components/experience.tsx
@@ -1,7 +1,43 @@
+import { ExperienceCard } from '@/components/experience-card'
+
+const experiences = [
+  {
+    name: 'Desenvolvedor Front-End',
+    type: 'Freelancer',
+    time: 'January 2025 - Until now',
+    description:
+      'Led the end-to-end development of a corporate website, from UI/UX design conception to front-end implementation. Focused on responsive design, high performance, and accessibility across all devices. Applied development best practices and version control using GitHub.',
+    skills: 'ReactJS, NextJS, TypeScript, TailwindCSS, ShadcnUI, Figma, GitHub',
+  },
+]
+
 export function Experience() {
   return (
-    <div className="flex items-center justify-center">
-      <h1>Experience</h1>
-    </div>
+    <section
+      id="experience"
+      className="min-h-screen w-full container mx-auto items-center p-4"
+    >
+      <div className="flex flex-col items-center justify-center p-4">
+        <h1 className="text-[48px] font-bold xl:text-[80px]">Experience</h1>
+        <p className="text-zinc-50/60 text-center text-base font-medium leading-tight md:leading-relaxed">
+          My professional journey as a front-end developer has allowed me to
+          work with diverse companies and projects, gaining valuable experience
+          across different technologies and industries.
+        </p>
+      </div>
+
+      {experiences.map((experience, index) => {
+        return (
+          <ExperienceCard
+            key={index}
+            name={experience.name}
+            type={experience.type}
+            time={experience.time}
+            description={experience.description}
+            skills={experience.skills}
+          />
+        )
+      })}
+    </section>
   )
 }
---
+++ src/components/experience-card.tsx
@@ -0,0 +1,58 @@
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
+
+type ExperienceCardProps = {
+  name: string
+  type: string
+  time: string
+  description: string
+  skills: string
+}
+
+export function ExperienceCard({
+  name,
+  type,
+  time,
+  description,
+  skills,
+}: ExperienceCardProps) {
+  const [expanded, setExpanded] = useState(false)
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="group flex flex-col gap-4 mt-9 w-full max-w-3xl border border-accent rounded-2xl p-4 bg-[#18181B] transition-all duration-500 hover:scale-[1.01]">
+        <div className="flex gap-4 items-start">
+          <div className="flex flex-col">
+            <h2 className="font-bold text-lg text-white">{name}</h2>
+            <p className="text-sm text-zinc-300 font-medium">{type}</p>
+            <span className="text-sm text-zinc-500">{time}</span>
+          </div>
+        </div>
+
+        <div>
+          <p className="text-zinc-100/60 font-medium">Resume:</p>
+          <p
+            className={`text-base mb-2 leading-relaxed text-zinc-200 ${
+              expanded ? '' : 'line-clamp-3'
+            } group-hover:line-clamp-none transition-all duration-300`}
+          >
+            {description}
+          </p>
+
+          <p className="text-zinc-100/60 font-medium">
+            Skills:{'  '}
+            <span className="text-zinc-200">{skills}</span>
+          </p>
+
+          <button
+            className="mt-2 text-sm text-accent font-medium md:hidden"
+            onClick={() => setExpanded(!expanded)}
+          >
+            {expanded ? 'Ver menos' : 'Ver mais'}
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
