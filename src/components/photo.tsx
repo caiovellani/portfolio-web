8442c942ab1cece5981df21c1aef2ba3c2ea7e2f
Caio Vellani <caiovellani@outlook.com>
2025-04-29 22:32:55 -0300

    fix: Adjust photo component positioning and spacing
    - Reduce padding from 6px to 2px in the photo container to improve layout. - Add responsive positioning from XL screens by adding transform classes that shift the SVG and image 20px left, with the image also moving 20px up.

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -6,9 +6,9 @@ import imgCaio from '@/../public/caio.png'
 
 export function Photo() {
   return (
-    <div className="relative w-[300px] h-[300px] xl:w-[380px] xl:h-[380px] flex items-center justify-center pointer-events-none overflow-hidden p-6">
+    <div className="relative w-[300px] h-[300px] xl:w-[380px] xl:h-[380px] flex items-center justify-center pointer-events-none overflow-hidden p-2">
       <motion.svg
-        className="absolute w-full h-full"
+        className="absolute w-full h-full xl:flex xl:transform xl:translate-x-[-20px]"
         fill="transparent"
         viewBox="0 0 506 506"
         xmlns="http://www.w3.org/2000/svg"
@@ -40,7 +40,7 @@ export function Photo() {
           opacity: 1,
           transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' },
         }}
-        className="relative w-[260px] h-[260px] xl:w-[330px] xl:h-[330px] mix-blend-lighten pointer-events-none"
+        className="relative w-[260px] h-[260px] xl:w-[330px] xl:h-[330px] mix-blend-lighten pointer-events-none xl:flex xl:transform xl:translate-x-[-20px] xl:translate-y-[-25px]"
       >
         <Image
           src={imgCaio}
